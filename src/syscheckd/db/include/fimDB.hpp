635996cee7037a985799c14f347ca208b6d41b89
Jotacarma <jotacarma90@gmail.com>
2021-10-07 12:20:54 +0200

    Added log functionality to FimDB class. Generalized log functions

--- src/syscheckd/db/include/fimDB.hpp
+++ src/syscheckd/db/include/fimDB.hpp
@@ -58,5 +58,10 @@ private:
     FIMDB(const FIMDB&) = delete;
     std::unique_ptr<DBSync>       m_dbsyncHandler;
     std::unique_ptr<RemoteSync>   m_rsyncHandler;
+    std::string createStatement();
+    void setFileLimit();
+    void setRegistryLimit();
+    void setValueLimit();
+
 };
 #endif //_FIMDB_HPP
